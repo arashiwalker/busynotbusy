42f1d95cfaf90216f9ceacb79f2c97dd5f399d24
arashi <arashiwalker@gmail.com>
2019-08-14 22:49:26 -0700

    firebase button

---
+++ assets/js/firebase.js
@@ -0,0 +1,15 @@
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyB3b6UavX4p0zug5iP5BnPas8kVcORjdfo",
+    authDomain: "busynotbusy-24279.firebaseapp.com",
+    databaseURL: "https://busynotbusy-24279.firebaseio.com",
+    projectId: "busynotbusy-24279",
+    storageBucket: "",
+    messagingSenderId: "810742588455",
+    appId: "1:810742588455:web:7c68e007157a72ad"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var db = firebase.database();
+</script>
